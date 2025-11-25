2a5fde4b59faf26aae2967676cb63205cebc7453
mp2-push <nanazapata7@yahoo.com>
2025-11-24 23:29:19 -0600

    First Commit

---
+++ data.py
@@ -0,0 +1,49 @@
+import data
+import helpers
+class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        #check if the Urban Routes server is reachable
+        if helpers.is_url_reachable(data.Urban_Routes_URL):
+            print("Connected to Urban Routes server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is still on and still running.")
+URBAN_ROUTES_URL = ''
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
+
+
+class TestUrbanRoutes:
+    def test_set_route(self):
+        # Add in s8
+        print("function created for set route")
+        pass
+    def test_select_plan(self):
+        # Add in s8
+        print("function created for select plan")
+        pass
+    def test_fill_phone_number(self):
+        # Add in s8
+        print("function created for fill phone number")
+        pass
+    def test_fill_card(self):
+        # Add in s8
+        print("function created for fill card")
+        pass
+    def test_comment_for_driver(self):
+        # Add s8
+        print("function created for comment for driver")
+        pass
+    def test_order_2_ice_cream(self):
+        # Looping twice for ordering 2 ice creams
+        for _ in range (2):
+        # Add in s8
+        pass
+    def test_car_search_model_appears(self):
+        # Add in s8
+        print("function created for car search model")
+        pass
